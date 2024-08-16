585d8bbb72ac88f22e941fe7715740624f9b4b93
phucnh7 <phucnh7@vng.com.vn>
2024-08-16 22:04:32 +0700

    chore update git access token

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
-git pull https://nguyenhongphuc98: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@github.com/Nguyenhongphuc98/StockDispatch.git
+git pull https://nguyenhongphuc98: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@github.com/Nguyenhongphuc98/StockDispatch.git
 docker-compose down
 docker rmi $(docker images -q)
 docker-compose build
 docker-compose up
+
+git pull https://nguyenhongphuc98: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@github.com/Nguyenhongphuc98/Export-manager-Web.git
