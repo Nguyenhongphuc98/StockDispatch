b69d3c92677bac24425fea2f8f8d1890e38dce2a
phucnh7 <phucnh7@vng.com.vn>
2024-08-31 19:57:47 +0700

    chore: update deploy cmd

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 git pull https://nguyenhongphuc98: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@github.com/Nguyenhongphuc98/StockDispatch.git
 docker-compose down
-docker rmi $(docker images -q)
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi -f $(docker images -q)
+docker volume rm $(docker volume ls -q)
+docker system prune -a --volumes
 docker-compose build
 docker-compose up
 
-git pull https://nguyenhongphuc98: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@github.com/Nguyenhongphuc98/Export-manager-Web.git
+# git pull https://nguyenhongphuc98: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@github.com/Nguyenhongphuc98/Export-manager-Web.git
+
