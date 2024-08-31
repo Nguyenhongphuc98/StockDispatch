0cd52135eec5e06038fd47efd8e352f1f9d5c85c
phucnh7 <phucnh7@vng.com.vn>
2024-08-31 20:05:24 +0700

    chore: update deploy cmd

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,7 @@ docker-compose down
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker rmi -f $(docker images -q)
-docker volume rm $(docker volume ls -q)
-docker system prune -a --volumes
+docker system prune -a
 docker-compose build
 docker-compose up
 
