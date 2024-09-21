21e9e1b119ada458a89b20a266b18972753663c2
phucnh7 <phucnh7@vng.com.vn>
2024-09-21 21:49:00 +0700

    chore: update system time

--- src/utils/system-time.ts
+++ src/utils/system-time.ts
@@ -5,13 +5,15 @@ class SystemTime {
 
     date() {
         const now = Date.now();
-        const delta = 7 * 60 * 60 * 1000;
+        // const delta = 7 * 60 * 60 * 1000;
+        const delta = 0;
         return new Date(now + delta);
     }
 
     now() {
         const now = Date.now();
-        const delta = 7 * 60 * 60 * 1000;
+        // const delta = 7 * 60 * 60 * 1000;
+        const delta = 0;
         return now + delta;
     }
 }
